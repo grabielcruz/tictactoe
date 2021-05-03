3c3cb8469d9b0d3fdb5b7161db9dee43982e61fc
Luis Cruz <luisgrabielcruz@gmail.com>
2021-05-03 09:20:01 -0400

    basic validation to inputs

--- src/main.js
+++ src/main.js
@@ -1,17 +1,31 @@
 const { prompt } = require('enquirer');
 const boardUtils = require('./boardUtils');
 
+const getPosition0to8 = async (user, board) => {
+  while (true) {
+    const response = await prompt({
+      type: 'input',
+      name: 'userPosition',
+      message: `Enter position ${user}:`,
+    });
+    if (Number(response.userPosition) >= 0 && Number(response.userPosition <= 8)) {
+      if (board[response.userPosition] === "X" || board[response.userPosition] === "O") {
+        console.log("Position already taken, choose a different one")
+        continue
+      }
+      return response.userPosition
+    }
+    console.log('Wrong position. It should be a number between 0 and 8')
+  }
+};
+
 const board = boardUtils.genBoard();
 let winner_checked = {};
-let response;
+
 const runGame = async () => {
   while (true) {
-    response = await prompt({
-      type: 'input',
-      name: 'user1position',
-      message: 'Enter position user1:',
-    });
-    boardUtils.markBoard(board, response.user1position, 'X');
+    const user1Position = await getPosition0to8("user1", board)
+    boardUtils.markBoard(board, user1Position, 'X');
     winner_checked = boardUtils.check_winner(board, 'X');
     console.log(board);
     if (winner_checked.type === 'success') {
@@ -19,12 +33,8 @@ const runGame = async () => {
       break;
     }
 
-    response = await prompt({
-      type: 'input',
-      name: 'user2position',
-      message: 'Enter position user2:',
-    });
-    boardUtils.markBoard(board, response.user2position, 'O');
+    const user2Position = await getPosition0to8("user2", board)
+    boardUtils.markBoard(board, user2Position, 'O');
     winner_checked = boardUtils.check_winner(board, 'O');
     console.log(board);
     if (winner_checked.type === 'success') {
